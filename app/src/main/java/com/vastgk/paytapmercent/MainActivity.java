9ecb36216c0ebaea9565e0a5ed9c34c69bbef532
Raj dubey <com.mailuser@gmail.com>
2019-10-12 10:11:22 +0530

    Intial Build

---
+++ app/src/main/java/com/vastgk/paytapmercent/MainActivity.java
@@ -0,0 +1,160 @@
+package com.vastgk.paytapmercent;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.util.Random;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+public class MainActivity extends AppCompatActivity {
+    private static final int MY_REQUEST_CODE_CAMERA =9 ;
+    HashMap<String,Double> ItemMap_Price=new HashMap<>();
+    HashMap<String,Integer> ItemMap_Quan=new HashMap<>();
+private final int CODE_SCAN=7;
+
+    private static final String TAG ="RAAJ" ;
+    Random random=new Random();
+ ListView itemView;
+ TextView DetailsTxtView;
+ Button scanCode,writePrice;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        itemView=findViewById(R.id.lstViewItems);
+        DetailsTxtView=findViewById(R.id.txtView_DetailsItem);
+        scanCode=findViewById(R.id.Btn_ScanCode);
+        writePrice=findViewById(R.id.Btn_writePrice);
+        scanCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    // Permission is not granted
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+                        ActivityCompat.requestPermissions(MainActivity.this,
+                                new String[]{Manifest.permission.CAMERA},
+                                MY_REQUEST_CODE_CAMERA);
+
+
+
+                }else
+                {
+                    RUNSCAN();
+                }
+
+
+
+
+
+            }
+        });
+        writePrice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"To be Implemented ",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // get the barcode reader instance
+    }
+    void Refreshdata()
+    {
+        ArrayList<String> details=new ArrayList<>();
+        double total_price=0.0;
+        for(String it:ItemMap_Price.keySet())
+        {
+            total_price+=(ItemMap_Quan.get(it)*ItemMap_Price.get(it));
+            String st="Item:"+it
+                    +"\tPrice: "
+                    +ItemMap_Quan.get(it)+"x"+ItemMap_Price.get(it)
+                    +"="+(ItemMap_Quan.get(it)*ItemMap_Price.get(it));
+            details.add(st);
+        }
+
+
+        ArrayAdapter arrayAdapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,details);
+        itemView.setAdapter(arrayAdapter);
+
+        DetailsTxtView.setText("Total Item "+details.size() +" Amounting Rs. "+total_price);
+    }
+    private void RUNSCAN()
+    {  Intent in=new Intent(MainActivity.this,Scan.class);
+        startActivityForResult(in,CODE_SCAN);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode==CODE_SCAN)
+        {
+            String ITEM_CODE=data.getStringExtra("code");
+
+
+        if(ItemMap_Price.containsKey(ITEM_CODE))
+        {
+
+            ItemMap_Quan.put(ITEM_CODE,ItemMap_Quan.get(ITEM_CODE)+1);
+           // ItemMap_Price.put(ITEM_CODE,ItemMap_Price.get(ITEM_CODE)*2);
+
+
+        }else
+        {  Double pr=random.nextDouble()*50;
+            ItemMap_Quan.put(ITEM_CODE,1);
+            ItemMap_Price.put(ITEM_CODE,Double.valueOf(String.format("%.2f",pr)));
+        }
+        Refreshdata();
+
+        }
+
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_REQUEST_CODE_CAMERA: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    Toast.makeText(getApplicationContext(),"Granted Permission ",Toast.LENGTH_SHORT).show();
+                    RUNSCAN();
+
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(getApplicationContext(),"NO CAMREA PERMISSION ",Toast.LENGTH_LONG).show();
+                    finish();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
+}
---
+++ app/src/main/java/com/vastgk/paytapmercent/Scan.java
@@ -0,0 +1,79 @@
+package com.vastgk.paytapmercent;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.AudioManager;
+import android.media.ToneGenerator;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.zxing.Result;
+
+
+public class Scan extends AppCompatActivity {
+    private CodeScanner mCodeScanner;
+private final int MY_REQUEST_CODE_CAMERA=9;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan);
+
+            RUNSCAN();
+
+
+
+
+    }
+
+
+    private void RUNSCAN()
+    {
+
+
+        CodeScannerView scannerView = findViewById(R.id.scanner_view);
+        mCodeScanner = new CodeScanner(this, scannerView);
+        mCodeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull final Result result) {
+                ToneGenerator toneGen1 = new ToneGenerator(AudioManager.STREAM_MUSIC, 100);
+                toneGen1.startTone(ToneGenerator.TONE_CDMA_PIP,150);
+                Intent intent=new Intent();
+                intent.putExtra("code",result.getText());
+                setResult(7,intent);
+
+                finish();
+            }
+        });
+        scannerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCodeScanner.startPreview();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mCodeScanner.startPreview();
+    }
+
+    @Override
+    protected void onPause() {
+        mCodeScanner.releaseResources();
+        super.onPause();
+    }
+
+}
