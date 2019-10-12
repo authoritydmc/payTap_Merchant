ad391f891125b6bb05b98c67c4278a34225a681c
Rohan <rohankumarchs@gmail.com>
2019-10-12 09:43:05 -0700

    change-BottomNavigation

--- app/src/main/java/com/vastgk/paytapmercent/MainActivity.java
+++ app/src/main/java/com/vastgk/paytapmercent/MainActivity.java
@@ -1,5 +1,6 @@
 package com.vastgk.paytapmercent;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -12,6 +13,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -19,114 +21,124 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
 
 public class MainActivity extends AppCompatActivity {
-    private static final int MY_REQUEST_CODE_CAMERA =9 ;
-    HashMap<String,Double> ItemMap_Price=new HashMap<>();
-    HashMap<String,Integer> ItemMap_Quan=new HashMap<>();
-private final int CODE_SCAN=7;
+    private static final int MY_REQUEST_CODE_CAMERA = 9;
+    HashMap<String, Double> ItemMap_Price = new HashMap<>();
+    HashMap<String, Integer> ItemMap_Quan = new HashMap<>();
+    private final int CODE_SCAN = 7;
 
-    private static final String TAG ="RAAJ" ;
-    Random random=new Random();
- ListView itemView;
- TextView DetailsTxtView;
- Button scanCode,writePrice;
+
+    private static final String TAG = "RAAJ";
+    Random random = new Random();
+    ListView itemView;
+    TextView DetailsTxtView;
+    Button scanCode, writePrice;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        itemView=findViewById(R.id.lstViewItems);
-        DetailsTxtView=findViewById(R.id.txtView_DetailsItem);
-        scanCode=findViewById(R.id.Btn_ScanCode);
-        writePrice=findViewById(R.id.Btn_writePrice);
-        scanCode.setOnClickListener(new View.OnClickListener() {
+        itemView = findViewById(R.id.lstViewItems);
+        DetailsTxtView = findViewById(R.id.txtView_DetailsItem);
+//        scanCode = findViewById(R.id.Btn_ScanCode);
+//        writePrice = findViewById(R.id.Btn_writePrice);
+        bottomNavigationView = findViewById(R.id.bottomnavigation);
+
+        //bottom Navigation ----------RK-------------
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            Intent intent;
             @Override
-            public void onClick(View v) {
-
-                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA)
-                        != PackageManager.PERMISSION_GRANTED) {
-                    // Permission is not granted
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
-                        ActivityCompat.requestPermissions(MainActivity.this,
-                                new String[]{Manifest.permission.CAMERA},
-                                MY_REQUEST_CODE_CAMERA);
-
-
-
-                }else
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId())
                 {
-                    RUNSCAN();
+                    case R.id.activity_main_bottomnavigation_scanner:
+                        scan();
+                        break;
+                    case R.id.activity_main_bottomnavigation_nfc:
+                        write();
+                        Toast.makeText(MainActivity.this, "Yet To Be Implemented", Toast.LENGTH_SHORT).show();
+                        break;
                 }
-
-
-
-
-
-            }
-        });
-        writePrice.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this,"To be Implemented ",Toast.LENGTH_SHORT).show();
+                return true;
             }
         });
 
-        // get the barcode reader instance
     }
-    void Refreshdata()
-    {
-        ArrayList<String> details=new ArrayList<>();
-        double total_price=0.0;
-        for(String it:ItemMap_Price.keySet())
-        {
-            total_price+=(ItemMap_Quan.get(it)*ItemMap_Price.get(it));
-            String st="Item:"+it
-                    +"\tPrice: "
-                    +ItemMap_Quan.get(it)+"x"+ItemMap_Price.get(it)
-                    +"="+(ItemMap_Quan.get(it)*ItemMap_Price.get(it));
+
+    private void write() {
+        Toast.makeText(MainActivity.this, "To be Implemented ", Toast.LENGTH_SHORT).show();
+    }
+
+    private void scan() {
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.CAMERA},
+                        MY_REQUEST_CODE_CAMERA);
+        } else {
+            RUNSCAN();
+        }
+    }
+
+
+    // get the barcode reader instance
+
+    void Refreshdata() {
+        ArrayList<String> details = new ArrayList<>();
+        double total_price = 0.0;
+        for (String it : ItemMap_Price.keySet()) {
+            total_price += (ItemMap_Quan.get(it) * ItemMap_Price.get(it));
+            String st = "Item:" + it
+                    + "\tPrice: "
+                    + ItemMap_Quan.get(it) + "x" + ItemMap_Price.get(it)
+                    + "=" + (ItemMap_Quan.get(it) * ItemMap_Price.get(it));
             details.add(st);
         }
 
 
-        ArrayAdapter arrayAdapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,details);
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, details);
         itemView.setAdapter(arrayAdapter);
 
-        DetailsTxtView.setText("Total Item "+details.size() +" Amounting Rs. "+total_price);
+        DetailsTxtView.setText("Total Item " + details.size() + " Amounting Rs. " + total_price);
     }
-    private void RUNSCAN()
-    {  Intent in=new Intent(MainActivity.this,Scan.class);
-        startActivityForResult(in,CODE_SCAN);
+
+    private void RUNSCAN() {
+        Intent in = new Intent(MainActivity.this, Scan.class);
+        startActivityForResult(in, CODE_SCAN);
 
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode==CODE_SCAN)
-        {
-            String ITEM_CODE=data.getStringExtra("code");
+        if (resultCode == CODE_SCAN) {
+            String ITEM_CODE = data.getStringExtra("code");
 
 
-        if(ItemMap_Price.containsKey(ITEM_CODE))
-        {
+            if (ItemMap_Price.containsKey(ITEM_CODE)) {
 
-            ItemMap_Quan.put(ITEM_CODE,ItemMap_Quan.get(ITEM_CODE)+1);
-           // ItemMap_Price.put(ITEM_CODE,ItemMap_Price.get(ITEM_CODE)*2);
+                ItemMap_Quan.put(ITEM_CODE, ItemMap_Quan.get(ITEM_CODE) + 1);
+                // ItemMap_Price.put(ITEM_CODE,ItemMap_Price.get(ITEM_CODE)*2);
 
 
-        }else
-        {  Double pr=random.nextDouble()*50;
-            ItemMap_Quan.put(ITEM_CODE,1);
-            ItemMap_Price.put(ITEM_CODE,Double.valueOf(String.format("%.2f",pr)));
-        }
-        Refreshdata();
+            } else {
+                Double pr = random.nextDouble() * 50;
+                ItemMap_Quan.put(ITEM_CODE, 1);
+                ItemMap_Price.put(ITEM_CODE, Double.valueOf(String.format("%.2f", pr)));
+            }
+            Refreshdata();
 
         }
-
 
 
     }
@@ -141,13 +153,13 @@ private final int CODE_SCAN=7;
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     // permission was granted, yay! Do the
                     // contacts-related task you need to do.
-                    Toast.makeText(getApplicationContext(),"Granted Permission ",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Granted Permission ", Toast.LENGTH_SHORT).show();
                     RUNSCAN();
 
                 } else {
                     // permission denied, boo! Disable the
                     // functionality that depends on this permission.
-                    Toast.makeText(getApplicationContext(),"NO CAMREA PERMISSION ",Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), "NO CAMREA PERMISSION ", Toast.LENGTH_LONG).show();
                     finish();
                 }
                 return;
@@ -157,4 +169,6 @@ private final int CODE_SCAN=7;
             // permissions this app might request.
         }
     }
+
+
 }
