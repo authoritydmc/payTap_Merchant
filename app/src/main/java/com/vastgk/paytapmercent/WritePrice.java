d10dac04a7d9f629ddaa5b213b9bff29721ca7e8
Raj dubey <com.mailuser@gmail.com>
2019-10-14 11:35:27 +0530

    Fixed Bug --causing WritePrice Activity TO crash

--- app/src/main/java/com/vastgk/paytapmercent/WritePrice.java
+++ app/src/main/java/com/vastgk/paytapmercent/WritePrice.java
@@ -16,7 +16,7 @@ private TextView dataTxtView;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write_price);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+       getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         dataTxtView=findViewById(R.id.write_nfc_data_txtView);
         String data_to_written= String.format("Vendor Name:%s| VENDOR ID=%s|AMOUNT=%s|CUST_ID=%s", VENDOR_NAME, VENDORD_ID, String.valueOf(getIntent().getDoubleExtra(TOTAL_AMOUNT_TO_WRITE,0.0)),"124578");
 
