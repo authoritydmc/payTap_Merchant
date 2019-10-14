e49868fbbc8bd0fe0231f56c1ecec3c0d17e31cd
Raj dubey <com.mailuser@gmail.com>
2019-10-14 10:10:13 +0530

    Write to NFC tag Activity added- TextView Show Data to be Written -merged changes to UI added ActivityBank

--- app/src/main/java/com/vastgk/paytapmercent/ActivityBank.java
+++ app/src/main/java/com/vastgk/paytapmercent/ActivityBank.java
@@ -10,5 +10,6 @@ public class ActivityBank extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bank);
+        //this is changes in ACTivity Bank ..hope fully you will get it
     }
 }
