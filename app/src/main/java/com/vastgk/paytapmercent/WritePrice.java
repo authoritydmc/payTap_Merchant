354bf7512084e0f905ff36ebcd3a02ed5a074bac
Raj dubey <com.mailuser@gmail.com>
2019-10-15 20:45:31 +0530

    ChangeLog: -Changed WritePrice mode to write in  JSON format

--- app/src/main/java/com/vastgk/paytapmercent/WritePrice.java
+++ app/src/main/java/com/vastgk/paytapmercent/WritePrice.java
@@ -6,11 +6,17 @@ import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONObject;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class WritePrice extends AppCompatActivity {
     public static String TOTAL_AMOUNT_TO_WRITE="amount_total_write";
     private String VENDOR_NAME="PAYTAP ";
     private String VENDORD_ID="VEN4578";
 private TextView dataTxtView;
+    private String CUSTOMER_ID="cust565447"; //TODO users mobile number
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +24,15 @@ private TextView dataTxtView;
         setContentView(R.layout.activity_write_price);
        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         dataTxtView=findViewById(R.id.write_nfc_data_txtView);
-        String data_to_written= String.format("Vendor Name:%s| VENDOR ID=%s|AMOUNT=%s|CUST_ID=%s", VENDOR_NAME, VENDORD_ID, getIntent().getStringExtra(TOTAL_AMOUNT_TO_WRITE),"124578");
+        Map map=new LinkedHashMap();
+        map.put("VendorName",VENDOR_NAME);
+        map.put("VendorId",VENDORD_ID);
+        map.put("Amount",getIntent().getStringExtra(TOTAL_AMOUNT_TO_WRITE));
+        map.put("Cust_ID",CUSTOMER_ID);
+        JSONObject datas=new JSONObject(map);
+        dataTxtView.setText(datas.toString());
 
-        dataTxtView.setText(data_to_written);
+
 
         Toast.makeText(this, "Touch the NFC tag to complete the writing", Toast.LENGTH_SHORT).show();
     }
